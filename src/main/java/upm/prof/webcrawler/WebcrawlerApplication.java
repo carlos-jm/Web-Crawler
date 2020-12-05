cfa3103ac5311f4490153a54c25fa9f14aba9975
Carlos <carlos.jimenez.martin@alumnos.upm.es>
2020-12-05 13:02:20 +0100

    Changes to deploy in Tomcat

--- src/main/java/upm/prof/webcrawler/WebcrawlerApplication.java
+++ src/main/java/upm/prof/webcrawler/WebcrawlerApplication.java
@@ -2,9 +2,10 @@ package upm.prof.webcrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebcrawlerApplication {
+public class WebcrawlerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebcrawlerApplication.class, args);
