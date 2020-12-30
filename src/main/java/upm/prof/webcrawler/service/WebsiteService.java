c5cd0a1c4a5088cf571ae0fdbb2f4e65ebfe272f
Andres Mareca <andres.mareca@gmail.com>
2020-12-30 13:19:48 +0100

    Add all WebsiteService tests

--- src/main/java/upm/prof/webcrawler/service/WebsiteService.java
+++ src/main/java/upm/prof/webcrawler/service/WebsiteService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 import upm.prof.webcrawler.repository.WebsiteRepository;
 import upm.prof.webcrawler.entity.Category;
 import upm.prof.webcrawler.entity.Type;
@@ -13,51 +14,43 @@ import upm.prof.webcrawler.exceptions.BadRequestException;
 
 @Service
 public class WebsiteService {
-	
-	@Autowired
-	public WebsiteRepository websiteRepository;
-	
-	@Autowired
-	private TypeService typeService;
-	@Autowired
-	private CategoryService categoryService;
-	
-	public List<Website> getAllDemoWebsites()
-	{
 
-		return websiteRepository.findAll();
-	}
-	
-	public Website getWebsiteById(int id) {
-		return websiteRepository.findWebsiteById(id);
-	}
-	
-	public Website saveWebsite(Website website) throws BadRequestException,AlreadyExistsException{
-		if(website.getUrl() == null || 
-				website.getType() == null ||
-				website.getCategory() == null ||
-				website.getLast_update() == null) 
-		{
-			
-			throw new BadRequestException("Rellene todos los campos obligatorios.");
-		
-		}
-		else if(websiteRepository.findWebsiteById(website.getId()) != null ) 
-		{
-			throw new AlreadyExistsException("Ya existe un usuario con ese id.");
-		}
-		
-		return websiteRepository.save(website);
-	}
-	
-	public void removeWebsiteById(int id) {
-		websiteRepository.deleteById(id);
-	}
-	
-	public List<Website> getFilterWebsite(String url, int cat_id, int type_id)
-	{
-		
-		return websiteRepository.filterWebsite("%"+url+"%", categoryService.getCategoryById(cat_id) , typeService.getTypeById(type_id));
-		
-	}
+    @Autowired
+    public WebsiteRepository websiteRepository;
+
+    @Autowired
+    public TypeService typeService;
+    @Autowired
+    public CategoryService categoryService;
+
+    public List<Website> getAllDemoWebsites() {
+        return websiteRepository.findAll();
+    }
+
+    public Website getWebsiteById(int id) {
+        return websiteRepository.findWebsiteById(id);
+    }
+
+    public Website saveWebsite(Website website) throws BadRequestException, AlreadyExistsException {
+        if (website.getUrl() == null ||
+                website.getType() == null ||
+                website.getCategory() == null ||
+                website.getLast_update() == null) {
+
+            throw new BadRequestException("Rellene todos los campos obligatorios.");
+
+        } else if (websiteRepository.findWebsiteById(website.getId()) != null) {
+            throw new AlreadyExistsException("Ya existe un usuario con ese id.");
+        }
+
+        return websiteRepository.save(website);
+    }
+
+    public void removeWebsiteById(int id) {
+        websiteRepository.deleteById(id);
+    }
+
+    public List<Website> getFilterWebsite(String url, int cat_id, int type_id) {
+        return websiteRepository.filterWebsite("%" + url + "%", categoryService.getCategoryById(cat_id), typeService.getTypeById(type_id));
+    }
 }
